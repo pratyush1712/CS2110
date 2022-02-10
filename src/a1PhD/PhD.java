c71056c4b439f5cfdd85ad42037cf62684a87401
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-02-09 19:52:50 -0500

    init

---
+++ src/a1PhD/PhD.java
@@ -0,0 +1,69 @@
+package a1PhD;
+/** NetId: ps2245, as2839. Time spent: hh hours, mm minutes. <br>
+ * What I thought about this assignment: <br>
+ * Replace this with your comment, using as many lines as you want. */
+
+ /** An instance maintains info about the PhD of a person. */
+
+
+public class PhD {
+	private String name;
+	private int month;
+	private int year;
+	private PhD advisorOne;
+	private PhD advisorTwo;
+	private int numAdvisees;
+	
+	public PhD(String n, int y, int m) {
+		name = n;
+		year = y;
+		month = m;
+		advisorOne = null;
+		advisorTwo = null;
+		numAdvisees = 0;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public String date() {
+		return String.valueOf(month)+"/"+String.valueOf(year);
+	}
+
+	public PhD advisor1() {
+		return advisorOne;
+	}
+	
+	public PhD advisor2() {
+		return advisorTwo;
+	}
+	
+	public int advisees() {
+		return numAdvisees;
+	}
+	
+	public void addAdvisor1(PhD p) {
+		advisorOne = p;
+		advisorOne.numAdvisees++;
+	}
+	
+	public void addAdvisor2(PhD p) {
+		advisorTwo = p;
+		advisorTwo.numAdvisees++;
+	}
+	
+	public PhD (String n, int y, int m, PhD p1, PhD p2) {
+		this(n,y,m);
+		advisorOne = p1;
+		advisorTwo = p2;
+	}
+	
+	public boolean gotBefore(PhD p) {
+		return (p.year > year) || (p.year == year && p.month>month);
+	}
+	
+	public boolean isSiblingOf(PhD p) {
+		return advisorOne==p.advisorOne || advisorTwo==p.advisorTwo;
+	}
+}
---
+++ src/a1PhD/PhDTest.java
@@ -0,0 +1,14 @@
+package a1PhD;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PhDTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
