6375ec03a32f43d205076a943517e588b965ae96
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2022-02-11 18:17:39 -0500

    Update PhDTest.java

--- src/a1/PhDTest.java
+++ src/a1/PhDTest.java
@@ -14,6 +14,8 @@ class PhDTest {
 		assertEquals(null, p1.advisor1());
 		assertEquals(null, p1.advisor2());
 		assertEquals(0, p1.advisees());
+		
+		
 	}
 	
 	@Test
@@ -27,6 +29,9 @@ class PhDTest {
 		p1.addAdvisor2(p3);
 		assertEquals(p3, p1.advisor2());
 		assertEquals(1, p3.advisees());
+		assertEquals(0, p1.advisees());
+		
+		
 	}
 	
 	@Test
@@ -40,36 +45,14 @@ class PhDTest {
 		assertEquals(p3, p1.advisor1());
 		assertEquals(1, p2.advisees());
 		assertEquals(1, p3.advisees());
+		
+		
 	}
 	
 	@Test
 	void testGroupD() {
-		PhD p1 = new PhD("Max Planck", 1946, 8);
-		PhD p2 = new PhD("Albert Einstein", 1905, 9);
-		assertEquals(true, p2.gotBefore(p1));
-		assertEquals(false, p1.gotBefore(p2));
-		
-		PhD p3 = new PhD("Caleb John", 1947, 8);
-		PhD p4 = new PhD("Robert Brady", 1947, 7);
-		assertEquals(true, p4.gotBefore(p3));
-		assertEquals(false, p3.gotBefore(p4));
-		
-		PhD p5 = new PhD("John Smith", 2002, 8, p1, p2);
-		PhD p6 = new PhD("John Geller", 2004, 6, p3, p4);
-		PhD p7 = new PhD("John Kennedy", 2005, 5, p1, p4);
-		assertEquals(false, p5.isSiblingOf(p6));
-		assertEquals(false, p6.isSiblingOf(p7));
-		assertEquals(true, p5.isSiblingOf(p7));
-		assertEquals(true, p6.isSiblingOf(p7));
-		assertEquals(false, p5.isSiblingOf(p5));
-
-	}
-	
-	@Test
-	void testAssertStatements() {
 		
 	}
-	
 	
 
 }
