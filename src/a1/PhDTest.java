9bc17d82cbe354692d9519c36d28f2805555afa6
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2022-02-11 17:38:30 -0500

    Update PhDTest.java

--- src/a1/PhDTest.java
+++ src/a1/PhDTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class PhDTest {
 
 	@Test
-	void testConstructor1() {
+	void testGroupA() {
 		PhD p1 = new PhD("Alex John", 2005, 7);
 		assertEquals("Alex John", p1.name());
 		assertEquals("7/2005", p1.date());
@@ -17,7 +17,7 @@ class PhDTest {
 	}
 	
 	@Test
-	void testMutators() {
+	void testGroupB() {
 		PhD p1 = new PhD("Alex John", 2005, 7);
 		PhD p2 = new PhD("Tim Boseman", 1985, 7);
 		PhD p3 = new PhD("Alex Shaun", 1994, 10);
@@ -30,7 +30,7 @@ class PhDTest {
 	}
 	
 	@Test
-	void testC() {
+	void testGroupC() {
 		PhD p2 = new PhD("Tim Boseman", 1985, 7);
 		PhD p3 = new PhD("Alex John", 1994, 10);
 		PhD p1 = new PhD("Alex Shaun", 2005, 7, p3, p2);
@@ -41,8 +41,33 @@ class PhDTest {
 	}
 	
 	@Test
-	void testD() {
+	void testGroupD() {
+		PhD p1 = new PhD("Max Planck", 1946, 8);
+		PhD p2 = new PhD("Albert Einstein", 1905, 9);
+		assertEquals(true, p2.gotBefore(p1));
+		assertEquals(false, p1.gotBefore(p2));
+		
+		PhD p3 = new PhD("Caleb John", 1947, 8);
+		PhD p4 = new PhD("Robert Brady", 1947, 7);
+		assertEquals(true, p4.gotBefore(p3));
+		assertEquals(false, p3.gotBefore(p4));
+		
+		PhD p5 = new PhD("John Smith", 2002, 8, p1, p2);
+		PhD p6 = new PhD("John Geller", 2004, 6, p3, p4);
+		PhD p7 = new PhD("John Kennedy", 2005, 5, p1, p4);
+		assertEquals(false, p5.isSiblingOf(p6));
+		assertEquals(false, p6.isSiblingOf(p7));
+		assertEquals(true, p5.isSiblingOf(p7));
+		assertEquals(true, p6.isSiblingOf(p7));
+		assertEquals(false, p5.isSiblingOf(p5));
+
+	}
+	
+	@Test
+	void testAssertStatements() {
 		
 	}
+	
+	
 
 }
