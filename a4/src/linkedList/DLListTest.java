e1318097616faacce312b879ba09d2c5e84e27fa
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2022-03-03 16:07:54 -0500

    Update DLListTest.java

--- a4/src/linkedList/DLListTest.java
+++ a4/src/linkedList/DLListTest.java
@@ -49,8 +49,10 @@ class DLListTest {
         dl.append("3360");
         dl.append("4999");
         assertEquals("2110", dl.value(dl.node(0)));
-        assertEquals("4999", dl.value(dl.node(5)));
-        assertEquals("3360", dl.value(dl.node(4)));
+        assertEquals("2800", dl.value(dl.node(1)));
+        assertEquals("2220", dl.value(dl.node(2)));
+        assertEquals("3360", dl.value(dl.node(3)));
+        assertEquals("4999", dl.value(dl.node(4)));
     }
 
     @Test
@@ -61,22 +63,18 @@ class DLListTest {
         dl.append("2220");
         dl.append("3360");
         dl.append("4999");
-        DLList<String> dll= new DLList<String>();
-        dl.insertBefore(dl.node(5), "syam math homework ho gaya?");
-        assertEquals("[2110, 2800, 2220, 3360, syam math homework ho gaya?, 4999]", dl.toString());
-        assertEquals("[4999, syam math homework ho gaya?, 3360, 2220, 2800, 2110]", dl.toStringRev());
+        dl.insertBefore(dl.node(4), "syam math homework ho gaya?");
+        assertEquals("[2110, 2800, 2220, 3360, 4999, syam math homework ho gaya?]", dl.toString());
+        assertEquals("[syam math homework ho gaya?, 4999, 3360, 2220, 2800, 2110]", dl.toStringRev());
         assertEquals(6, dl.size());
-        dl.insertBefore(dl.node(1), "syam math homework bhej de");
-        assertEquals("[syam math homework bhej de, 2110, 2800, 2220, 3360, syam math homework ho gaya?, 4999]", dl.toString());
-        assertEquals("[4999, syam math homework ho gaya?, 3360, 2220, 2800, 2110, syam math homework bhej de]", dl.toStringRev());
-        assertEquals(7, dl.size());
+        DLList<String> dll= new DLList<String>();
         dll.append("3");
         dll.append("8");
         dll.append("2");
-        dll.insertBefore(dll.node(2), "1");
-        assertEquals("[3, 8, 1, 2]", dll.toString());
-        
-    
+        dll.insertBefore(dll.node(0), "1");
+        assertEquals("[3, 1, 8, 2]", dll.toString());
+        assertEquals("[2, 8, 1, 3]", dll.toStringRev());
+        assertEquals(4, dll.size());
     }
 
 }
